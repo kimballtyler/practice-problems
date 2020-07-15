c9bb593d3307509c430041be4a490ddf2926e69b
Tyler Kimball <kimballstyler@gmail.com>
2020-07-15 13:16:10 -0400

    Completed pp get word count

---
+++ pp-get-word-count/main.js
@@ -0,0 +1,7 @@
+function getWordCount(sentence) {
+  return sentence.split(" ").length;
+}
+//test
+console.log(getWordCount("This is a sample sentence"))
+console.log(getWordCount("Han shot first"))
+console.log(getWordCount("I will not fear, fear is the mind killer"))
